26ed394488563d42194f23dd613be7a5e8b20e04
Rishat Suleymanov <spk_irbis@mail.ru>
2021-03-12 20:15:17 +0300

    Refactoring

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,7 @@
 package ru.netology;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Box box = new Box();
         Toy toy = new Toy(box);
         Gamer gamer = new Gamer(box);
